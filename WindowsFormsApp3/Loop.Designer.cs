b37db700c4c31e9770f3700e11ca78602d447da1
GioChyru <www.sed.ge@gmail.com>
2019-07-10 10:45:28 +0400

    test1

---
+++ WindowsFormsApp3/Loop.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Loop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.PictureBox();
+            this.loopGif2 = new System.Windows.Forms.PictureBox();
+            this.loopGif4 = new System.Windows.Forms.PictureBox();
+            this.loopGif5 = new System.Windows.Forms.PictureBox();
+            this.loopGif3 = new System.Windows.Forms.PictureBox();
+            this.loopGif1 = new System.Windows.Forms.PictureBox();
+            this.loopGif6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox38 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox38)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.Image = global::WindowsFormsApp3.Properties.Resources.close;
+            this.closeButton.Location = new System.Drawing.Point(389, 20);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(13, 13);
+            this.closeButton.TabIndex = 44;
+            this.closeButton.TabStop = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // loopGif2
+            // 
+            this.loopGif2.BackColor = System.Drawing.Color.Transparent;
+            this.loopGif2.Image = global::WindowsFormsApp3.Properties.Resources.Circle;
+            this.loopGif2.Location = new System.Drawing.Point(235, 144);
+            this.loopGif2.Name = "loopGif2";
+            this.loopGif2.Size = new System.Drawing.Size(156, 156);
+            this.loopGif2.TabIndex = 74;
+            this.loopGif2.TabStop = false;
+            this.loopGif2.Paint += new System.Windows.Forms.PaintEventHandler(this.loopGif_Paint);
+            // 
+            // loopGif4
+            // 
+            this.loopGif4.BackColor = System.Drawing.Color.Transparent;
+            this.loopGif4.Image = global::WindowsFormsApp3.Properties.Resources.Circle;
+            this.loopGif4.Location = new System.Drawing.Point(235, 344);
+            this.loopGif4.Name = "loopGif4";
+            this.loopGif4.Size = new System.Drawing.Size(156, 156);
+            this.loopGif4.TabIndex = 75;
+            this.loopGif4.TabStop = false;
+            this.loopGif4.Paint += new System.Windows.Forms.PaintEventHandler(this.loopGif_Paint);
+            // 
+            // loopGif5
+            // 
+            this.loopGif5.BackColor = System.Drawing.Color.Transparent;
+            this.loopGif5.Image = global::WindowsFormsApp3.Properties.Resources.Circle;
+            this.loopGif5.Location = new System.Drawing.Point(37, 547);
+            this.loopGif5.Name = "loopGif5";
+            this.loopGif5.Size = new System.Drawing.Size(156, 156);
+            this.loopGif5.TabIndex = 76;
+            this.loopGif5.TabStop = false;
+            this.loopGif5.Paint += new System.Windows.Forms.PaintEventHandler(this.loopGif_Paint);
+            // 
+            // loopGif3
+            // 
+            this.loopGif3.BackColor = System.Drawing.Color.Transparent;
+            this.loopGif3.Image = global::WindowsFormsApp3.Properties.Resources.Circle;
+            this.loopGif3.Location = new System.Drawing.Point(37, 344);
+            this.loopGif3.Name = "loopGif3";
+            this.loopGif3.Size = new System.Drawing.Size(156, 156);
+            this.loopGif3.TabIndex = 78;
+            this.loopGif3.TabStop = false;
+            this.loopGif3.Paint += new System.Windows.Forms.PaintEventHandler(this.loopGif_Paint);
+            // 
+            // loopGif1
+            // 
+            this.loopGif1.BackColor = System.Drawing.Color.Transparent;
+            this.loopGif1.Image = global::WindowsFormsApp3.Properties.Resources.Circle;
+            this.loopGif1.Location = new System.Drawing.Point(37, 144);
+            this.loopGif1.Name = "loopGif1";
+            this.loopGif1.Size = new System.Drawing.Size(156, 156);
+            this.loopGif1.TabIndex = 77;
+            this.loopGif1.TabStop = false;
+            this.loopGif1.Paint += new System.Windows.Forms.PaintEventHandler(this.loopGif_Paint);
+            // 
+            // loopGif6
+            // 
+            this.loopGif6.BackColor = System.Drawing.Color.Transparent;
+            this.loopGif6.Image = global::WindowsFormsApp3.Properties.Resources.Circle;
+            this.loopGif6.Location = new System.Drawing.Point(235, 547);
+            this.loopGif6.Name = "loopGif6";
+            this.loopGif6.Size = new System.Drawing.Size(156, 156);
+            this.loopGif6.TabIndex = 79;
+            this.loopGif6.TabStop = false;
+            this.loopGif6.Paint += new System.Windows.Forms.PaintEventHandler(this.loopGif_Paint);
+            // 
+            // pictureBox38
+            // 
+            this.pictureBox38.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox38.Image = global::WindowsFormsApp3.Properties.Resources.minimise;
+            this.pictureBox38.Location = new System.Drawing.Point(370, 20);
+            this.pictureBox38.Name = "pictureBox38";
+            this.pictureBox38.Size = new System.Drawing.Size(13, 13);
+            this.pictureBox38.TabIndex = 80;
+            this.pictureBox38.TabStop = false;
+            this.pictureBox38.Click += new System.EventHandler(this.pictureBox38_Click);
+            // 
+            // Loop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImage = global::WindowsFormsApp3.Properties.Resources.LoopBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(429, 819);
+            this.Controls.Add(this.pictureBox38);
+            this.Controls.Add(this.loopGif6);
+            this.Controls.Add(this.loopGif3);
+            this.Controls.Add(this.loopGif1);
+            this.Controls.Add(this.loopGif5);
+            this.Controls.Add(this.loopGif4);
+            this.Controls.Add(this.loopGif2);
+            this.Controls.Add(this.closeButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Loop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loop";
+            this.TransparencyKey = System.Drawing.SystemColors.InactiveBorder;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Loop_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Loop_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Loop_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loopGif6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox38)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox closeButton;
+        private System.Windows.Forms.PictureBox loopGif2;
+        private System.Windows.Forms.PictureBox loopGif4;
+        private System.Windows.Forms.PictureBox loopGif5;
+        private System.Windows.Forms.PictureBox loopGif3;
+        private System.Windows.Forms.PictureBox loopGif1;
+        private System.Windows.Forms.PictureBox loopGif6;
+        private System.Windows.Forms.PictureBox pictureBox38;
+    }
+}
